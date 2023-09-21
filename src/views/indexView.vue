df17bebecae48b81310dd71821d18924be7d1a14
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-21 15:07:13 +0900

    fix words

--- src/views/indexView.vue
+++ src/views/indexView.vue
@@ -2,7 +2,7 @@
 .wrap
   v-card.content(elevation="4")
     .text-h1 VueTemp {{ PackageJson.version }}
-    .text-h6 Vuetifyを簡単に最速に構築できるサンプル
+    .text-h6 Vuetifyを簡単に構築できるサンプル
     .btns
       v-btn(@click="a('https://github.com/jikantoki/vuetifytemplate')") Github
       v-btn(@click="console.log(setCookie('test', '001'))") Add Cookie
