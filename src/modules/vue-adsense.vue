d514f31cca09269e4cc36a48a3c66ec25d3a1ed7
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-22 01:07:20 +0900

    リファクタ

--- src/modules/vue-adsense.vue
+++ src/modules/vue-adsense.vue
@@ -4,7 +4,6 @@ ins.adsbygoogle(
   :data-ad-slot="adSlot"
   :data-ad-format="adFormat"
   :style="adStyle")
-v-btn(@click="height()") 調整
 </template>
 
 <script>
@@ -32,16 +31,7 @@ export default {
   mounted() {
     1 + 1
     ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-    let wrap = document.getElementsByClassName('v-application__wrap')[0]
-    let cssText = wrap.style.cssText
-    wrap.style.cssText = cssText + 'min-height: 100vh!important'
   },
-  methods: {
-    height() {
-      let wrap = document.getElementsByClassName('v-application__wrap')[0]
-      let cssText = wrap.style.cssText
-      wrap.style.cssText = cssText + 'min-height: 100vh!important'
-    }
-  }
+  methods: {}
 }
 </script>
