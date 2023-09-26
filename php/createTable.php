8d03f568e8566e4e7bcc18e5f537fe83b9cf55dd
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-27 02:26:02 +0900

    update

--- php/createTable.php
+++ php/createTable.php
@@ -2,5 +2,11 @@
 
 require_once './settings.php';
 require_once './functions/database.php';
+$test = SQLshowTable();
+var_dump($test);
 $test = SQLsearchTable('usersList');
+var_dump($test);
+$test = SQLcreateTable('usersList');
+var_dump($test);
+$test = SQLshowTable();
 var_dump($test);
