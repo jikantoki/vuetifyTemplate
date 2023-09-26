35da0a84cc833cb14ab79f944509fd29ebc342b6
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-27 02:39:58 +0900

    update

--- php/createTable.php
+++ php/createTable.php
@@ -2,13 +2,14 @@
 
 require_once './settings.php';
 require_once './functions/database.php';
+require_once './functions/functions.php';
 $test = SQLshowTable();
-var_dump($test);
+dump($test);
 $test = SQLsearchTable('usersList');
-var_dump($test);
+dump($test);
 if (!$test) {
   $test = SQL('create table usersList (id int, name varchar(10), col varchar(10))');
-  var_dump($test);
+  dump($test);
 }
 $test = SQLshowTable();
-var_dump($test);
+dump($test);
---
+++ php/functions/functions.php
@@ -0,0 +1,19 @@
+<?
+
+/**
+ * エラーが出ず、最後に改行されるvar_dump
+ *
+ * @param [*] $var
+ * @return int
+ */
+function dump($var)
+{
+  try {
+    var_dump($var);
+  } catch (Exception $e) {
+    echo $e . '\n';
+    return 1;
+  }
+  echo '\n';
+  return 0;
+}
