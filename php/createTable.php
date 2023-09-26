c8d65d8b70d59d2ee33d7485131d856a0c0fa9d5
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-27 02:35:23 +0900

    update

--- php/createTable.php
+++ php/createTable.php
@@ -6,7 +6,9 @@ $test = SQLshowTable();
 var_dump($test);
 $test = SQLsearchTable('usersList');
 var_dump($test);
-$test = SQL('create table usersList (id int, name varchar(10), col varchar(10))');
-var_dump($test);
+if (!$test) {
+  $test = SQL('create table usersList (id int, name varchar(10), col varchar(10))');
+  var_dump($test);
+}
 $test = SQLshowTable();
 var_dump($test);
