6a5decb2506a5afcd30d8c6b454f3a8cc89f96e4
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-27 01:28:32 +0900

    smallfix

--- php/functions/database.php
+++ php/functions/database.php
@@ -6,7 +6,6 @@ require_once '../../env.php';
 
 $sql = 'mysql:host=' . MySQL_Host . ';dbname=' . MySQL_DBName . ';charset=utf8mb4';
 try{
-  echo $sql;
   $pdo = new PDO($sql, MySQL_User, MySQL_Password);
 } catch(Exception $e) {
   echo $e->getMessage();
