2b24171157f3c8a9f06f600f2b47ef4163a76e77
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-27 17:30:13 +0900

    makeapi

--- php/makeApiForAdmin.php
+++ php/makeApiForAdmin.php
@@ -8,21 +8,22 @@ const API_LIST_FORVIEW = 'api_listForView';
 const API_SECRET_ID = 'secretId';
 $availableApiList = SQLsearchTable(API_LIST);
 if ($availableApiList) {
-  $rand = SQLmakeRandomId(API_LIST, API_SECRET_ID);
+  //$rand = SQLmakeRandomId(API_LIST, API_SECRET_ID);
   //api_listテーブルにランダムなAPIトークンを作成
   $apiToken = randomString(16);
   $apiAccessKey = randomString(16);
   $apiAccessKeyHashed = password_hash($apiAccessKey, PASSWORD_DEFAULT);
-  echo SQLinsert(API_LIST, [
-    API_SECRET_ID => $rand,
-    'apiId' => $rand,
+  SQLinsert(API_LIST, [
+    API_SECRET_ID => 'default',
+    'apiId' => 'default',
     'apiToken' => $apiToken,
     'apiAccessKey' => $apiAccessKeyHashed
   ]);
-  echo SQLinsert(API_LIST_FORVIEW, [
-    API_SECRET_ID => $rand,
+  SQLinsert(API_LIST_FORVIEW, [
+    API_SECRET_ID => 'default',
     'apiName' => 'webapp'
   ]);
+  echo 'apiId = "default"<br>apiToken = "' . $apiToken . '"<br>apiAccessKey = "' . $apiAccessKey . '"';
 } else {
   echo 'DBのセットアップを先にやってね！';
 }
