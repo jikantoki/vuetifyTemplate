d607e444b8b430b496cbe1b7dcab6af5331fd4df
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-28 02:07:42 +0900

    test

--- php/sendPush.php
+++ php/sendPush.php
@@ -1,6 +1,6 @@
 <?php
-require_once '/vendor/autoload.php';
-require_once '/env.php';
+require_once './vendor/autoload.php';
+require_once './env.php';
 
 use Minishlink\WebPush\WebPush;
 use Minishlink\WebPush\Subscription;
